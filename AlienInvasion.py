310848af3bcbe8d21da1574eb7edfb1c2042c271
danjor667 <jordannguepi@gmail.com>
2023-08-26 13:45:31 +0200

    fixing the game boundaries

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -27,11 +27,21 @@ class AlienInvasion():
                     self.ship.moving_right = True
                 elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = True
+                elif event.key == pygame.K_q:
+                    sys.exit()
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
                 if event.key == pygame.K_LEFT:
                     self.ship.moving_left = False
+                if event.key == pygame.K_UP:
+                    self.ship.moving_up = False
+                if event.key == pygame.K_DOWN:
+                    self.ship.moving_down = False
 
     def _update_screen(self):
         self.screen.fill(self.bg_color)
--- ship.py
+++ ship.py
@@ -24,17 +24,27 @@ class Ship():
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
+        self.moving_down = False
 
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
     def update(self):
-        if self.moving_right:
-            if self.rect.right < self.screen_rect.right:
-                self.x += self.settings.ship_speed
-        if self.moving_left:
-            if self.rect.left > 0:
-                self.x -= self.settings.ship_speed
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+            """
+            behaving as its in the cosine part of
+            the x-y plane. dont know why
+            """
+        if self.moving_up and self.rect.y > self.screen_rect.top:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+
         self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         """
